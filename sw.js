2fd1cf8e0959c9dcea47e0eb2fa0876d90ad061d
samanthajopetersen-ai <samanthajopetersen@gmail.com>
2026-09-07 09:23:41 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'mads-dinos-v1';
+const ASSETS = [
+  './',
+  './Mad\'s%20Dinos.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
