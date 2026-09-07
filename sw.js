0932a6f3c0de635335a6de9b1a37e9b7b2a470d5
samanthajopetersen-ai <samanthajopetersen@gmail.com>
2026-09-07 10:17:44 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mads-dinos-v1';
+const CACHE_NAME = 'mads-dinos-v2';
 const ASSETS = [
   './',
   './index.html',
@@ -8,8 +8,23 @@ const ASSETS = [
 ];
 
 self.addEventListener('install', (event) => {
+  self.skipWaiting(); // Forces new service worker to activate immediately
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key); // Deletes the old broken layout cache
+          }
+        })
+      );
+    }).then(() => self.clients.claim()) // Takes control of all open app tabs immediately
   );
 });
 
